c940399e1d50b8e45bffaafc1563a4c94c56b553
Grayson Myers <graymyers10@gmail.com>
2021-02-12 10:44:19 -0700

    Add name instance variable

--- app/poros/character.rb
+++ app/poros/character.rb
@@ -1,6 +1,7 @@
 class Character
-  attr_reader :affiliation, :enemies, :allies
+  attr_reader :affiliation, :enemies, :allies, :name
   def initialize(data)
+    @name = data[:name]
     @affiliation = data[:affiliation]
     @enemies = data[:enemies]
     @allies = data[:allies]
