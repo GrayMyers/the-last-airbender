22e67e880ca188d939dcbf98bedb69ba9690d4c7
Grayson Myers <graymyers10@gmail.com>
2021-02-12 10:45:22 -0700

    Update feature test to work with selector field

--- spec/features/fire_nation_spec.rb
+++ spec/features/fire_nation_spec.rb
@@ -3,11 +3,14 @@ require "rails_helper"
 describe "fire nation page" do
   describe "(happy path)" do
     it "allows users to search for members" do
-      visit search_path
+      visit root_path
+      find('#nation').find(:xpath, 'option[3]').select_option
+      click_on "Search For Members"
+
       response = TheLastAirbenderFacade.get_nation_info("Fire+Nation")
       characters = response[:characters]
       population = response[:population]
-      expect(page).to have_content("Population: "+population)
+      expect(page).to have_content("Population: #{population}")
 
       characters.each do |character|
         expect(page).to have_content(character.name)
