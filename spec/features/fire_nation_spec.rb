d69a60f96e455bf231a454c4fb298e98259d85bf
Grayson Myers <graymyers10@gmail.com>
2021-02-12 09:19:06 -0700

    Update spec to match user story

--- spec/features/fire_nation_spec.rb
+++ spec/features/fire_nation_spec.rb
@@ -3,13 +3,10 @@ require "rails_helper"
 describe "fire nation page" do
   describe "(happy path)" do
     it "allows users to search for members" do
-      visit fire_nation_path
+      visit search_path
 
-      click_button "Search For Members"
-
-      expect(current_path).to eq(search_path)
       expect(page).to have_content(TheLastAirbenderService.population_count("fire"))
-      
+
     end
   end
 end
