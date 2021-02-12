e6b84201d4a02db6091fd8ec14e26b1e2d5165bd
Grayson Myers <graymyers10@gmail.com>
2021-02-12 10:48:35 -0700

    Refactor views to use helper to format enemies and allies arrays

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
-    def element_nation
+  def element_nation
     [
       ['Air Nomads', 'Air+Nomads'],
       ['Earth Kingdom', 'Earth+Kingdom'],
       ['Fire Nation', 'Fire+Nation'],
       ['Water Tribes', 'Water+Tribe'],
     ]
+  end
+
+  def format_relations(relations)
+    if relations.count > 0
+      relations * ", "
+    else
+      "None"
+    end
   end
 end
