4a04b42b0d20a155b41130e4bd719c376020fb90
Grayson Myers <graymyers10@gmail.com>
2021-02-12 09:54:22 -0700

    Fix issue with test and make testing more intensive

--- spec/services/last_airbender_spec.rb
+++ spec/services/last_airbender_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "The last airbender service" do
   describe "(happy path)" do
     it "can get info for a nation" do
-      results = TheLastAirbenderService.get_nation_info("Fire+Nation")
+      results = TheLastAirbenderService.get_nation_info("Fire+Nation",25)
       population = results[:population]
       characters = results[:characters]
 
@@ -11,6 +11,7 @@ describe "The last airbender service" do
 
       expect(population).to be_an(Integer)#expect(subject.first).to have_key(:revenue)
       expect(characters).to be_a(Array)
+      expect(characters.count).to eq(25)
 
       expect(first_character).to have_key(:allies)
       expect(first_character).to have_key(:enemies)
